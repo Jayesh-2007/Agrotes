bd066a7300ca689fd69f86204f7f8a0b51b1f08e
Jayesh Hadiyal <jayeshhadiyal2007@gmail.com>
2026-01-30 16:56:25 +0530

    today learn about the how to create dynamic page with ejs library

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,23 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
+const path = require("path");
+
+//setting view engine to the ejs
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 app.get("/", (req, res) => {
-  let code = "<h1>Fruit Info</h1>";
-  code += "<p>Fruit: apple</p>";
-  code += "<p>Color: red</p>";
-  res.send(code);
-  //   res.send({
-  //     fruit: 'apple',
-  //     color: 'red'
-  //   res.send('hello world');
+  res.render("home.ejs");
+});
+
+app.get("/hello",(req, res) =>{
+  res.send("Hello!");
+})
+
+app.get("/rolldice",(req, res) =>{
+  let diceVal = Math.floor(Math.random() * 6) + 1;
+  res.render("rolldice.ejs", {diceVal});
 });
 
 app.get("/:username/:id", (req, res) => {
@@ -19,14 +26,14 @@ app.get("/:username/:id", (req, res) => {
   res.send(`Hello, ${username}! Your ID is ${id}`);
 });
 
-app.get("/search",(req, res)=>{
-  let {q} = req.query;
-  if(!q){
+app.get("/search", (req, res) => {
+  let { q } = req.query;
+  if (!q) {
     res.send("no search query provided");
     return;
   }
   res.send(`search results for: ${q}`);
-})
+});
 
 app.get("/apple", (req, res) => {
   res.send("apple page");
@@ -39,7 +46,6 @@ app.get("/help", (req, res) => {
 app.post("/", (req, res) => {
   res.send("You sent a Post request");
 });
-
 
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`);
